6ee08cecf879b4d55466e2e5f1d4e57466ba0c1d
Nolan <meyer09@gmail.com>
2024-12-03 17:54:09 -0600

    add ndim to create_grid

--- test_vector_grid.py
+++ test_vector_grid.py
@@ -5,11 +5,13 @@ from vector_grid import create_grid
 
 
 def test_create_grid():
-    grid = create_grid(3, 3)
-    assert grid.shape == (3, 3)
+    grid = create_grid(3, 3, 1)
+    assert grid.shape == (3, 3, 1)
+    grid = create_grid(3, 3, 2)
+    assert grid.shape == (3, 3, 2)
 
 def test_random_values():
-    grid = create_grid(1000, 1000)
+    grid = create_grid(1000, 1000, 2)
     assert grid.min() >= -1
     assert grid.max() <= 1
     assert np.allclose(grid.mean(), 0, atol = 0.05), "Mean is not approximately zero"
--- vector_grid.py
+++ vector_grid.py
@@ -1,13 +1,25 @@
 import numpy as np
 from config import GRID_WIDTH, GRID_HEIGHT
+import matplotlib.pyplot as plt
 
 def main():
-    grid = create_grid(GRID_WIDTH, GRID_HEIGHT)
+    grid = create_grid(GRID_WIDTH, GRID_HEIGHT, 2)
+    points = grid.reshape(-1, 2)
+    x = points[:, 0]
+    y = points[:, 1]
+    fig, ax = plt.subplots()
+    ax.scatter(x, y)
+    # Save the plot to a file
+    output_image = "scatter_plot.png"
+    plt.savefig(output_image)
+    print(f"Scatter plot saved as {output_image}")
 
-def create_grid(width, height):
-    grid_shape = (width, height)
-    grid = (np.random.default_rng(seed=22).random(size=(grid_shape))*2)-1
+
+def create_grid(width, height, ndim=1):
+    grid_shape = (width, height, ndim)
+    grid = (np.random.default_rng().random(size=(grid_shape))*2)-1
     return grid
 
 if __name__ == "__main__":
     main()
+
